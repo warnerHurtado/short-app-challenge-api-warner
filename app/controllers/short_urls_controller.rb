3fc8e8ccbda36ac8243bb274b4804e67dd1b634a
warnerHurtado <warner3898@gmail.com>
2022-11-04 17:06:24 -0600

    feat(backend): save autogenerate url_code in database

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -4,15 +4,18 @@ class ShortUrlsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
+    url = ShortUrl.all
+    render json: { test: url }
   end
 
   def create
     url = ShortUrl.find_by( full_url: params[:full_url] )
-    if url.save
-      render status: 200, json: { short_code: url } 
+    if url
+      render status: 200, json: { short_code: url.short_code } 
     else
       url = ShortUrl.create( full_url: params[:full_url] )
-      render status: 201, json: { short_code: url }
+
+      render status: 201, json: { short_code: url.short_code }
     end
   end
 
--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -5,6 +5,17 @@ class ShortUrl < ApplicationRecord
   validate :validate_full_url
 
   def short_code
+    number = ShortUrl.all.length
+    url_code = ""
+    while number > 0
+      result = number % 62
+      url_code.prepend( CHARACTERS[result] )
+      number = ( number / 62 ).floor
+    end
+    url = ShortUrl.find_by(full_url: full_url)
+    url.url_code = url_code
+    url.save
+    url_code
   end
 
   def update_title!
