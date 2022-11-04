13430e1e9a093161dd80dfec04f2e057efc84255
warnerHurtado <warner3898@gmail.com>
2022-11-04 16:09:44 -0600

    feat(backend): validate if url already exists

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -7,8 +7,13 @@ class ShortUrlsController < ApplicationController
   end
 
   def create
+    url = ShortUrl.find_by( full_url: params[:full_url] )
+    if url.save
+      render status: 200, json: { short_code: url } 
+    else
       url = ShortUrl.create( full_url: params[:full_url] )
       render status: 201, json: { short_code: url }
+    end
   end
 
   def show
